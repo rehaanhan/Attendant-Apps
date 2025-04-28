b3667a5cc97ccb356197c6e8fb0639892efbf3cd
rehaanhan <raihanmuabrok811@gmai.com>
2025-04-28 19:59:47 +0700

    added .gitignore and training data

---
+++ train_emotion_cnn.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+
+# Data paths
+train_dir = 'dataset/train'
+test_dir = 'dataset/test'
+
+# Image preprocessing
+img_size = 48
+batch_size = 64
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(img_size, img_size),
+    batch_size=batch_size,
+    color_mode="grayscale",
+    class_mode='categorical'
+)
+
+test_data = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(img_size, img_size),
+    batch_size=batch_size,
+    color_mode="grayscale",
+    class_mode='categorical'
+)
+
+# Build model
+model = Sequential([
+    Conv2D(64, (3,3), activation='relu', input_shape=(img_size, img_size, 1)),
+    BatchNormalization(),
+    MaxPooling2D(2,2),
+    
+    Conv2D(128, (3,3), activation='relu'),
+    BatchNormalization(),
+    MaxPooling2D(2,2),
+    
+    Conv2D(256, (3,3), activation='relu'),
+    BatchNormalization(),
+    MaxPooling2D(2,2),
+    
+    Flatten(),
+    Dense(256, activation='relu'),
+    Dropout(0.5),
+    Dense(7, activation='softmax')  # 7 kelas emosi
+])
+
+# Compile
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train
+model.fit(
+    train_data,
+    validation_data=test_data,
+    epochs=20
+)
+
+# Save model
+if not os.path.exists("model"):
+    os.makedirs("model")
+
+model.save("model/emotion_model.h5")
+print("Model saved!")
